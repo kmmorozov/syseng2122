8fda6983b11d727fc99aaa45805985cfb6783e32
Kirill Morozov <kmmorozov@gmail.com>
2022-01-24 19:47:10 +0300

    edit upr_serv

--- py2022/proj1/upr_serv.py
+++ py2022/proj1/upr_serv.py
@@ -1,12 +1,36 @@
-def get_connect_data():
-    print('123')
-    return conn_data
+import configparser
 
-def my_pluss(a,b):
-    c = a + b
-    return c 
+def get_connect_data(serv_file):
+    server_dict = {}
+    sf = open(serv_file,'r')
+    for line in sf:
+        data_list = line.split(':')
+        server_dict.update({data_list[0]:data_list})
+    return server_dict
+        
+    
+
+
+def get_path():
+    config = configparser.ConfigParser()
+    config.read('general.conf')
+    ser_file = config['global']['servers']
+    com_file = config['global']['commands']
+    return ser_file, com_file
+
+    
 
 if __name__ == '__main__':
+    server_file, command_file = get_path()
+    print(server_file)
+    print(command_file)
+    
+    connect_data = get_connect_data(server_file)
+    print(type(connect_data))
+    print(connect_data)
+    
+    
+    
+    
+    
 
-    summ = my_pluss(10,15)
-    print(summ)
