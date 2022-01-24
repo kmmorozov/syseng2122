0784edcdcc1971a59c1a37ed83f9080b2b95fd5d
Kirill Morozov <kmmorozov@gmail.com>
2022-01-24 19:23:38 +0300

    add general.conf

---
+++ py2022/proj1/upr_serv.py
@@ -0,0 +1,12 @@
+def get_connect_data():
+    print('123')
+    return conn_data
+
+def my_pluss(a,b):
+    c = a + b
+    return c 
+
+if __name__ == '__main__':
+
+    summ = my_pluss(10,15)
+    print(summ)
