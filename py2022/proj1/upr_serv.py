62847a03e2df23d77f96b5ffcc1c88f327530f40
Kirill Morozov <kmmorozov@gmail.com>
2022-01-25 19:43:39 +0300

    pep8

--- py2022/proj1/upr_serv.py
+++ py2022/proj1/upr_serv.py
@@ -4,23 +4,21 @@ import paramiko
 
 def get_connect_data(serv_file):
     server_dict = {}
-    sf = open(serv_file,'r')
+    sf = open(serv_file, 'r')
     for line in sf:
         data_list = line.split(':')
-        server_dict.update({data_list[0]:data_list})
+        server_dict.update({data_list[0]: data_list})
     sf.close()
     return server_dict
 
 
 def get_commands(comm_file):
     commands = []
-    cf = open(comm_file,'r')
+    cf = open(comm_file, 'r')
     for line in cf:
-        #print(line)
         commands.append(line.strip())
     cf.close()
     return commands
-    
 
 
 def get_path():
@@ -30,38 +28,26 @@ def get_path():
     com_file = config['global']['commands']
     return ser_file, com_file
 
-def push_commands_to_server(server,user,passwd,commands):
+
+def push_commands_to_server(server, user, passwd, commands):
     print(server, user, passwd, '!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
     client = paramiko.SSHClient()
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    client.connect(server, '22', user, passwd, look_for_keys=False, allow_agent=False)
-    #client.connect(server, '22' , 'root', '123qaz', compress=True)
-    #client.connect(hostname, port, username, password, compress=True)
-    #stdin, stdout, stderr = client.exec_command('ls /')
+    client.connect(server, '22', user, passwd,
+                   look_for_keys=False, allow_agent=False)
     for command in commands:
         stdin, stdout, stderr = client.exec_command(command)
         print('=================================================')
         for line in stdout:
             print('... ' + line.strip('\n'))
         print('=================================================')
-        
-    
-    
-    
-    
 
 if __name__ == '__main__':
     server_file, command_file = get_path()
     connect_data = get_connect_data(server_file)
-    #print(connect_data)
     commands = get_commands(command_file)
     servers = connect_data.keys()
     for server in servers:
-       username = (connect_data[server][1]).strip()
-       password = (connect_data[server][2]).strip()
-       push_commands_to_server(server.strip(),username,password,commands)
-    
-    
-    
-    
-
+        username = (connect_data[server][1]).strip()
+        password = (connect_data[server][2]).strip()
+        push_commands_to_server(server.strip(), username, password, commands)
